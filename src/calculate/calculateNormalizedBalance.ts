c672017b66407b9f545d92bb7cbbe08e3cbb15e3
kames <kamescg@gmail.com>
2022-01-24 13:14:28 -0700

    linting

--- src/calculate/calculateNormalizedBalance.ts
+++ src/calculate/calculateNormalizedBalance.ts
@@ -1,6 +1,5 @@
 import { BigNumber } from "ethers";
 
 export function calculateNormalizedBalance(balance: BigNumber, ticketTotalSupply: BigNumber) {
-    console.log(balance, ticketTotalSupply);
     return balance.div(ticketTotalSupply);
 }
